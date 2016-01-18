a34bf6f8e169cab6990b33b352e3595b447f4b49
devopspilot <devopspilot@gmail.com>
2016-01-18 13:20:15 +0000

    corrected appstart.bat fine

--- DevOpsPilot/appstart.bat
+++ DevOpsPilot/appstart.bat
@@ -1,4 +1,5 @@
 @echo off
 pushd  "C:\apache-tomcat-7.0.67\bin"
+call shutdown.bat
 call startup.bat
-start ""http://localhost:8080/DevopsPilot/welcome.jsp
+start http://localhost:8080/DevopsPilot/welcome.jsp
