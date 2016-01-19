259fdcd905a59c3fda8234b2de4b66ef311dfc1f
devopspilot <devopspilot@gmail.com>
2016-01-19 13:27:26 +0000

    aa

--- DevOpsPilot/appspecscripts/appstart.bat
+++ DevOpsPilot/appspecscripts/appstart.bat
@@ -1,8 +1,2 @@
-@echo on
-#start powershell
-pushd  "C:\apache-tomcat-7.0.67"
-cd bin
-.\shutdown.bat
-cd..
-start cmd /k "C:\apache-tomcat-7.0.67\bin\startup.bat
+start cmd /k "C:\apache-tomcat-7.0.67\bin\startup.bat"
 exit
---
+++ DevOpsPilot/appspecscripts/appstop.bat
@@ -0,0 +1,2 @@
+start cmd /k "C:\apache-tomcat-7.0.67\bin\shutdown.bat"
+exit
