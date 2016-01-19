9f9bc67d04c92002c110ded044ebcf2b6e6c48b5
devopspilot <devopspilot@gmail.com>
2016-01-19 12:10:15 +0000

    as

--- DevOpsPilot/appspecscripts/appstart.bat
+++ DevOpsPilot/appspecscripts/appstart.bat
@@ -1,8 +1,7 @@
-@echo off
+@echo on
 pushd  "C:\apache-tomcat-7.0.67\bin"
 call shutdown.bat
 SLEEP 4
-call startup.bat
-SLEEP 30
-start http://localhost:8080/DevopsPilot/welcome.jsp
+call .\startup.bat
+@echo "exiting.."
 exit
