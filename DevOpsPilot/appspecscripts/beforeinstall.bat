42be75134d18c193e66d143a7d6344a23e5c00ce
devopspilot <devopspilot@gmail.com>
2016-01-19 11:10:59 +0000

    ssa

--- DevOpsPilot/appspecscripts/beforeinstall.bat
+++ DevOpsPilot/appspecscripts/beforeinstall.bat
@@ -1,5 +1,13 @@
 @echo off
 pushd  "C:\apache-tomcat-7.0.67\webapps"
-rm DevopsPilot
-del DevopsPilot.war
+$strFolderName="DevopsPilot"
+If (Test-Path $strFolderName){
+	Remove-Item $strFolderName
+}
+
+$strFileName=DevopsPilot.war
+If (Test-Path $strFileName){
+	Remove-Item $strFileName
+}
+
 TIMEOUT 5
