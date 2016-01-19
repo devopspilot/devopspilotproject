24e555a02507a777a47c1b38f71628b3c0cca5e2
devopspilot <devopspilot@gmail.com>
2016-01-19 11:47:56 +0000

    ss

--- DevOpsPilot/appspecscripts/beforeinstall.bat
+++ DevOpsPilot/appspecscripts/beforeinstall.bat
@@ -1,14 +1,8 @@
 @echo off
 pushd  "C:\apache-tomcat-7.0.67\webapps"
 
-@REM Folder to be deleted
-SET FolderToDelete="DevopsPilot"
-if exist %FolderToDelete% rd /q /s %FolderToDelete%
+if exist "DevopsPilot" rd /q /s "DevopsPilot"
 
-@REM File to be deleted
-SET FileToDelete="DevopsPilot.war"
- 
-@Try to delete the file only if it exists
-IF EXIST %FileToDelete% del /F %FileToDelete%
+if exist "DevopsPilot.war" del /F "DevopsPilot.war"
 
 TIMEOUT 5
